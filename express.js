050769646b5918433f48979c258aaaab3fddc4c8
Melvin Sackaria <melvin.sackaria@chathurangam.net>
2014-04-15 15:31:51 -0700

    qwert

--- express.js
+++ express.js
@@ -5,7 +5,7 @@ var consolidate = require('consolidate');
 var MongoClient = mongodb.MongoClient;
 
 stashed change check pishkoooooo
-piskkkk12323213213121
+piskkkk12323213213121CHANGE911223
 
 MongoClient.connect('mongodb://CMP:CMP@localhost/CMP', function (err, db) {
     if (err) throw err;
