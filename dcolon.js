599965836b5560c42d21e4d0df745575aa4af3f2
Melvin Sackaria <melvin.sackaria@chathurangam.net>
2014-04-15 17:33:11 -0700

    d colon

--- dcolon.js
+++ dcolon.js
@@ -1,5 +1,6 @@
  DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDA
   $scope.init = function() {
+  console.log('----------------DDDDDDDDDDD colon Function---------------------');
    console.log('----------------D colon Function---------------------');
     $scope.find();
   };
